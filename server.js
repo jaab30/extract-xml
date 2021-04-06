19d0a0cc63d24b7f69e8ba9f2df867e9d7db24c7
jaab30 <jaab30@yahoo.com>
2021-04-06 12:38:36 -0400

    added another method to convert xml files

--- server.js
+++ server.js
@@ -20,27 +20,55 @@ const path = require('path');
 const util = require('util');
 const gunzip = require('gunzip-file')
 const parser = require('xml2json');
+var xml2js = require('xml2js');
 
 const compFolder = path.join(__dirname, 'assets/comp-files/');
 const decompFolder = path.join(__dirname, 'assets/decomp-files/');
 const gzip = util.promisify(gunzip)
 
-
-const extractAndConvert = async (fileName) => {
+const extractAndConvertA = async (fileName) => {
     try {
         // extract .gz file
         await gzip(compFolder + fileName, decompFolder + 'extract.xml')
         // Read converted File
         const data = fs.readFileSync(decompFolder + 'extract.xml')
         // Convert xml to JSON
+        console.log('converting to JSON...');
         var json = parser.toJson(data, {object: true});
         // Console log content
-        console.log(JSON.stringify(json, null, 2));
+        console.log(util.inspect(json, false, null))
+        // console.log(JSON.stringify(json, null, 2));
+
     } catch(err) {
         console.log(err);
     }
 }
    
-extractAndConvert('EFUS-RelNotes-A.xml.gz');
+extractAndConvertA('EFUS-RelNotes-A.xml.gz');
 // This one take a bit longer since it is a big file
-// extractAndConvert('EFUS-INC-A.xml.gz');
+// extractAndConvertA('EFUS-INC-A.xml.gz');
+
+
+
+//******** USING ANOTHER "xml2js" PACKAGE TO CONVERT TO JSON */
+
+const extractAndConvertB = async (fileName) => {
+    try {
+        // extract .gz file
+        await gzip(compFolder + fileName, decompFolder + 'extract.xml')
+        // Read converted File
+        const data = fs.readFileSync(decompFolder + 'extract.xml')
+        // Convert xml to JSON
+        console.log('converting to JSON...');
+        let parser = new xml2js.Parser(/* options */);
+        let result = await parser.parseStringPromise(data)
+        // Console log content
+        console.log(util.inspect(result, false, null))
+        // console.dir(result);
+        // console.log(JSON.stringify(result, null, 2));
+    } catch(err) {
+        console.log(err);
+    }
+}
+// extractAndConvertB('EFUS-RelNotes-A.xml.gz');
+// extractAndConvertB('EFUS-INC-A.xml.gz');
